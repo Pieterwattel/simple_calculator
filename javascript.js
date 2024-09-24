2a188e911d31504ab60e70377ab801d8d37ad22c
Pieter_W <163425907+Pieterwattel@users.noreply.github.com>
2024-09-24 19:03:56 +0200

    make index and overview in JS file

---
+++ javascript.js
@@ -0,0 +1,71 @@
+/*
+===========
+>>>INDEX<<<
+===========
+
+> VARIABLE DECLARATIONS
+    >> NODE DECLARATIONS		
+        >>> NUMBERS
+        >>> OPERATORS
+        >>> OTHER NODE DECLARATION
+    >> OTHER GLOBAL VARIABLES
+> FUNCTIONS
+    >> UTILITY FUNCTIONS
+    >> CONTROLLER FUNCTIONS
+> DOM GENERATION
+    >> NODE CREATION
+    >> LISTENERS
+*/
+
+//> VARIABLE DECLARATIONS
+//#region
+//    >> NODE DECLARATIONS	
+//#region
+//        >>> NUMBERS
+//#region
+
+
+//#endregion
+//        >>> OPERATORS
+//#region
+
+
+//#endregion
+//        >>> OTHER NODE DECLARATIONS
+//#region
+
+
+//#endregion
+//#endregion
+//    >> OTHER GLOBAL VARIABLES
+//#region
+
+
+//#endregion
+//#endregion
+//> FUNCTIONS
+//#region
+//    >> UTILITY FUNCTIONS
+//#region
+
+
+//#endregion
+//    >> CONTROLLER FUNCTIONS
+//#region
+
+
+//#endregion
+//#endregion
+//> DOM GENERATION
+//#region
+//    >> NODE CREATION
+//#region
+
+
+//#endregion
+//    >> LISTENERS
+//#region
+
+
+//#endregion
+//#endregion
